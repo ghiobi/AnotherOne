bd02c7629af52b41c5c603abc2fe207edb7d9b65
DoubleBond <lamlaurendy@gmail.com>
2016-01-22 17:00:37 -0500

    Added sound

--- webroot/resources/js/profile.js
+++ webroot/resources/js/profile.js
@@ -3,6 +3,8 @@
  */
 $(function(){
     $('.semester-title').click(function(){
+        var sound = new Audio('http://localhost:8000/resources/sound/cut.mp3');
+        sound.play();
         if(!$(this).next().hasClass('semester-data-active')){
             $('.semester-data-active').slideUp(200).removeClass('semester-data-active');
             $(this).next().slideDown(200).addClass('semester-data-active');
