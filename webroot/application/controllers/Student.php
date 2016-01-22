7ba5a552e191b921ef0559e1b7067b1fa76e8b8e
mosquitodawg <eric.payette24@gmail.com>
2016-01-22 03:53:51 -0500

    Revert "Added CSS from profile.html to profile.php"
    This reverts commit ca117afd4cc129cc63a5774034ee6e111eeb38a7.

--- webroot/application/controllers/Student.php
+++ webroot/application/controllers/Student.php
@@ -14,15 +14,15 @@ class Student extends App_Base_Controller
 	function profile(){
 		//TODO: Fill in the blank
 		//$this->load->model('____');
-		
+
+
 		//TODO: fill in the blank
 		//if(//__________->post('______')){
 			//TODO: load model that deals with users for password
 			//$this->load->model('____');
 			//TODO: check password. etc...
 		//}
-		
-		//TODO: Display the name of the person signed in ,from database, instead of dank meme
+
 		$data['info_bar'] = 'AND HIS NAME IS JOHN CENA';
 		$this->load->view('layouts/header.php', $data);
 		//TODO edit profile php. Make it dynamic to the content it should be displaying.
