e054ac7fb4030da9e4ac9afdba7d2a2e4d0c8756
ronnpang1 <ronn.pang@gmail.com>
2016-01-29 11:27:36 -0500

    Added Scheduler model

---
+++ webroot/application/models/SchedulerProfile.php
@@ -0,0 +1,31 @@
+<?php 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+*  Works with the login controller to authenticates users and retrieve user data
+*/
+class User extends CI_Model
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	}
+	
+	function enroll_courses()
+	{
+		
+		
+		
+	}
+	
+	function generate_schedule()
+	{
+		
+		
+		
+		
+		
+	}
+	?>
