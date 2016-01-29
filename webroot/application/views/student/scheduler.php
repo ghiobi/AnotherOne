16d6a1971f2af7219a2b2683012a1548b7289eea
Michael Mescheder <meschedermichael@gmail.com>
2016-01-29 11:17:31 -0500

    Version 3.02.02 Added Time Step
    Minutes now increment by 5 minute intervals.

--- webroot/application/views/student/scheduler.php
+++ webroot/application/views/student/scheduler.php
@@ -151,11 +151,11 @@
 						<div class="form-group">
 							<label class="col-sm-2 control-label">Interval:</label>
 							<div class="col-sm-5">
-								<input type="time" class="form-control time_interval"/>
+								<input type="time" class="form-control time_interval" step="300"/>
 								<div class="help-block">Starting time.</div>
 							</div>
 							<div class="col-sm-5">
-								<input type="time" class="form-control time_interval"/>
+								<input type="time" class="form-control time_interval" step="300"/>
 								<div class="help-block">Ending time.</div>
 							</div>
 						</div>
